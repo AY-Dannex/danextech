e8b838817904dcdda4f9bff63b9d0ccb198d4c91
AY-Dannex <ayomidedaniel1212@gmail.com>
2025-11-05 20:33:10 +0100

    Changed the vhiktonyImg src in carousel component

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -2,7 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import vhiktonyImg from '../assets/vhiktonyImg.webp'
+import vhiktonyImg from '../assets/vhiktonyimg.webp'
 import sophia from '../assets/sophia.webp'
 import michael from '../assets/michael.webp'
 import chloe from '../assets/chloe.webp'
